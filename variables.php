859ddfa2f690cdd2c995c07b05a587221d1ab2a6
Saptarshi Mandal <80155876+Saptarshi1104@users.noreply.github.com>
2023-09-21 17:02:04 +0530

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php
+// echo "This is Tutorial 5";
+// Variables are containers for storing information
+// Starts with a $
+// Variables are case sensitive
+$name = "Saptarshi";
+$NAME = "SAPTARSHI";
+$income = 200.8;
+echo "This person is $name and his income is Rs. $income <br>";
+echo "$name is a good boy";
+?>
